a13b1b852ba54033940663b0b4ef448a17706931
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-02-19 22:40:52 -0300

    list/delete produtos

--- hello-world-spring/src/main/java/controller/ProdutoController.java
+++ hello-world-spring/src/main/java/controller/ProdutoController.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ import persistence.ProdutoRepository;
  *
  * @author iapereira
  */
-@RestController
+@Controller
 @RequestMapping(value = "/produtos")
 @ComponentScan("persistence.")
 public class ProdutoController {     
