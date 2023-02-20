2308a1e51a3636740e1e48b55f68e20b0596dc15
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-02-20 15:40:04 -0300

    list/delete produtos

--- ferragem/src/main/java/controller/HomeController.java
+++ ferragem/src/main/java/controller/HomeController.java
@@ -4,10 +4,8 @@
  */
 package controller;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
